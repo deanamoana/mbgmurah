6b45ad8b41881b4dc1cd7bd93d1b16268228be13
andeananda <dearizkyananda1@gmail.com>
2026-06-09 04:57:13 +0700

    aduhh

--- index.js
+++ index.js
@@ -1,58 +1,10 @@
-require('dotenv').config(); 
+require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors');
-const sequelize = require('./config/db');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
-
-// Import semua routes
-const dapurRoutes = require('./routes/dapurRoutes');
-const menuRoutes = require('./routes/menuRoutes');
-const sekolahRoutes = require('./routes/sekolahRoutes');
-const inventoryRoutes = require('./routes/inventoryRoutes');
-const shipmentRoutes = require('./routes/shipmentRoutes');
-
 const app = express();
-const PORT = process.env.PORT || 3000; // Gunakan satu port saja
-
-// Middleware   
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Swagger Configuration (Gabungan info bisa disesuaikan)
-const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API MBG Barokah - Integrated Service',
-            version: '1.0.0',
-            description: 'Dokumentasi API Terpadu untuk Program MBG',
-        },
-        servers: [{ url: `http://localhost:${PORT}` }],
-    },
-    apis: ['./routes/*.js'], 
-};
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-// Routes
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/api/dapur', dapurRoutes);
-app.use('/api/menu', menuRoutes);
-app.use('/api/sekolah', sekolahRoutes);
-app.use('/api/inventory', inventoryRoutes);
-app.use('/api/distribusi', shipmentRoutes);
 
 app.get('/', (req, res) => {
-    res.send(`🚀 Server Utama MBG berjalan di port ${PORT}`);
+    res.send('Vercel OK');
 });
-
-sequelize.sync({ alter: true })
-    .then(() => {
-        console.log('✅ Database terkoneksi');
-    })
-    .catch(err => {
-        console.error('❌ Gagal sinkronisasi database:', err.message);
-    });
 
 module.exports = app;
