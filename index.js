14e791b50aa1222ab60d994e2b5a4fd5fa966b89
andeananda <dearizkyananda1@gmail.com>
2026-06-09 05:00:42 +0700

    waduhh

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/db');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Vercel OK');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        res.send('Database Connected');
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 module.exports = app;
